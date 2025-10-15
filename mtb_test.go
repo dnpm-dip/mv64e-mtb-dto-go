c8134c402364abf78b9a9f81ecccf4f3cd5e15ba
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-10-15 08:21:56 +0200

    test: serde timezone usage

--- mtb_test.go
+++ mtb_test.go
@@ -2,6 +2,8 @@ package mtb
 
 import (
 	_ "embed"
+	"encoding/json"
+	"strings"
 	"testing"
 )
 
@@ -13,5 +15,13 @@ func TestShouldDeserializeJson(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Cannot deserialize MTB file")
+	}
+}
+
+func TestShouldKeepTimezone(t *testing.T) {
+	mtb, _ := UnmarshalMtb(fakeMtbData)
+	actual, err := json.Marshal(mtb)
+	if err != nil || !strings.Contains(string(actual), "\"birthDate\":\"1985-05-19\"") {
+		t.Errorf("Date does not match. Timezone issues?")
 	}
 }
