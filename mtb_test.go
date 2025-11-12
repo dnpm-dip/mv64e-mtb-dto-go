cd01fab18104b3cfb4afb9bc709cc74d132f52b6
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-11-12 10:28:37 +0100

    feat: convert date format for patient data

--- mtb_test.go
+++ mtb_test.go
@@ -19,15 +19,28 @@ func TestShouldDeserializeJson(t *testing.T) {
 	}
 }
 
-func TestShouldKeepTimezone(t *testing.T) {
-	re := regexp.MustCompile("\"birthDate\":\"\\d{4}-\\d{2}-\\d{2}\"")
+func TestShouldKeepPatientBirthdateFormatInYearMonth(t *testing.T) {
+	re := regexp.MustCompile("\"birthDate\":\"\\d{4}-\\d{2}(-\\d{2})?\"")
 
-	expectedDate := re.FindAllStringSubmatch(string(fakeMtbData), -1)[0][0]
+	alteredMtbData := re.ReplaceAll(fakeMtbData, []byte("2025-03"))
 
-	mtb, _ := UnmarshalMtb(fakeMtbData)
+	mtb, _ := UnmarshalMtb(alteredMtbData)
 	actual, err := json.Marshal(mtb)
 
-	if err != nil || !strings.Contains(string(actual), expectedDate) {
+	if err != nil || strings.Contains(string(actual), `"birthDate":"2025-03"`) {
+		t.Errorf("Date does not match. Timezone issues?")
+	}
+}
+
+func TestShouldConvertPatientBirthdateFormatInYearMonth(t *testing.T) {
+	re := regexp.MustCompile("\"birthDate\":\"\\d{4}-\\d{2}(-\\d{2})?\"")
+
+	alteredMtbData := re.ReplaceAll(fakeMtbData, []byte("2025-03-19"))
+
+	mtb, _ := UnmarshalMtb(alteredMtbData)
+	actual, err := json.Marshal(mtb)
+
+	if err != nil || strings.Contains(string(actual), `"birthDate":"2025-03"`) {
 		t.Errorf("Date does not match. Timezone issues?")
 	}
 }
